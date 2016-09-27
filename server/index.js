f7f45821ceff3a27f31175a8c8ab95448339d457
TinLe - Home <levinhtin@gmail.com>
2016-09-28 02:32:39 +0700

    Setup project: Render component

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 import express from 'express';
 import path from 'path';
+
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackConfig from '../webpack.config.dev.js';
+
+
 let app = express();
+app.use(webpackMiddleware(webpack(webpackConfig)));
 
 app.get('/*', (req, res) => {
   // res.send('Hello World');
