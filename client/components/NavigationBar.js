7b1afd300c547f63c20716e086f84a53c8662550
TinLe - Home <levinhtin@gmail.com>
2016-09-29 01:44:05 +0700

    Basic state

--- client/components/NavigationBar.js
+++ client/components/NavigationBar.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-export default () => {
-  return(
-    <nav className="navbar navbar-default">
-      <div className="container-fluid">
-        <div className="navbar-header">
-          <Link to="/" className="navbar-brand">Home</Link>
-        </div>
+class NavigationBar extends React.Component {
+  render() {
+    return(
+      <nav className="navbar navbar-default">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <Link to="/" className="navbar-brand">Home</Link>
+          </div>
 
-        <div className="collapse navbar-collapse">
-          <ul className="nav navbar-nav navbar-right">
-            <li>
-              <Link to="/signup" className="navbar-brand">Sign up</Link>
-            </li>
-          </ul>
+          <div className="collapse navbar-collapse">
+            <ul className="nav navbar-nav navbar-right">
+              <li>
+                <Link to="/signup" className="navbar-brand">Sign up</Link>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
-  );
+      </nav>
+    )
+  }
 }
+
+export default NavigationBar;
